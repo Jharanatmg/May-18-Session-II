560f0673d1297f7beda64f0a1c6957c4349737d9
Jharanatmg <jharanatmg22@gmail.com>
2023-05-18 20:33:05 -0700

    is it done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <header>Header</header>
+      <main><h1>This is <mark>heading 1</mark></h1>
+      <h2>This is <mark>heading 2</mark></h2></main>
+      <footer>Footer</footer>
+
+    </div>
+        
+    </>
+  )
+}
+
+export default App
